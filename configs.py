a6b73c781b6adbd3d9de8b21dd048fe147c40c4b
dubuqingfeng <1135326346@qq.com>
2016-05-19 22:37:20 +0800

    update mongodb config

--- configs.py
+++ configs.py
@@ -5,4 +5,6 @@ from pymongo import MongoClient
 __author__ = 'qingfeng'
 
 THEME = 'default/'
-client = MongoClient('mongodb://admin:mypass@192.168.99.100:27017/')
+client = MongoClient('mongodb://%s:%s@%s:%s/' % (
+    os.getenv('MONGODB_USER', 'admin'), os.getenv('MONGODB_PASS', 'mypass'),
+    os.getenv('MONGODB_PORT_27017_TCP_ADDR', '192.168.99.100'), os.getenv('MONGODB_PORT_27017_TCP_PORT', '27017')))
